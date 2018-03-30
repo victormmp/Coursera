653de56b6b6ee9ae17c0a52032ea4bbaa73e71f1
Victor Magalhães <victormmp@users.noreply.github.com>
2018-03-30 13:16:39 -0300

    Initial Upload

---
+++ Discrete Optimization/Assignment 1/anyint/solver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from random import *
+
+def solve_it(input_data):
+    # return a positive integer, as a string
+    return str(randint(7,10))
+
+if __name__ == '__main__':
+    print('This script submits the integer: %s\n' % solve_it(''))
+
